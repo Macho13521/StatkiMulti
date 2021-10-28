96fac30429985529311988776c6b8ed1416ba2ee
Macho13521 <36113176+Macho13521@users.noreply.github.com>
2021-10-28 19:45:52 +0200

    Dodanie rozstawiania statków
    Gracz Nr.2 może rozstawić swoje statki

--- StatkiMulti.cpp
+++ StatkiMulti.cpp
@@ -30,6 +30,7 @@ void Mapa(int gracz) {
             }
             cout << endl;
         }
+        cout << endl;
     }
     else
     {
@@ -40,20 +41,16 @@ void Mapa(int gracz) {
             }
             cout << endl;
         }
+        cout << endl;
     }
 }
 
-int main()
+void Rozstawienie1()
 {
+    progres = 1;
+
     int kord1 = 10;
     int kord2 = 10;
-
-
-    cout << "Wpisz nazwe pierwszego gracza" << endl;
-    cin >> imie1;
-
-    cout << "Wpisz nazwe drugiego gracza" << endl;
-    cin >> imie2;
 
     while (progres < ilejednopolowych + 1)
     {
@@ -80,6 +77,8 @@ int main()
             cout << "Tam juz jest statek!" << endl;
         }
     }
+
+
 
     progres = 1;
     int kierunek;
@@ -212,10 +211,194 @@ int main()
         }
 
     }
+}
 
+
+void Rozstawienie2()
+{
+    progres = 1;
+
+    int kord1 = 10;
+    int kord2 = 10;
+
+    while (progres < ilejednopolowych + 1)
+    {
+        Sleep(3000);
+        system("cls");
+        Mapa(2);
+
+        cout << "Niech gracz " << imie2 << " wpisze koordynaty gdzie chce miec statek Nr: " << progres << " jednopolowy. Odzielone spacja." << endl;
+        cin >> kord1 >> kord2;
+        if (gracz2[kord1 - 1][kord2 - 1] == 0)
+        {
+            if (11 > kord1 - 1 >= 0 && 11 > kord2 - 1 >= 0)
+            {
+                gracz2[kord1 - 1][kord2 - 1] = 1;
+                progres++;
+            }
+            else
+            {
+                cout << "Nie mozna umiescic statku poza mapa!" << endl;
+            }
+        }
+        else
+        {
+            cout << "Tam juz jest statek!" << endl;
+        }
+    }
+
+
+
+    progres = 1;
+    int kierunek;
+    int kord1v2;
+    int kord2v2;
+
+    bool statekv2gut = false;
+
+    while (progres < iledwupolowych + 1)
+    {
+        Sleep(3000);
+        system("cls");
+        Mapa(2);
+
+        cout << "Niech gracz " << imie2 << " wpisze koordynaty statku Nr: " << progres << " dwupolowego. Odzielone spacja." << endl;
+        cin >> kord1 >> kord2;
+
+        if (gracz2[kord1 - 1][kord2 - 1] == 0)
+        {
+            if (11 > kord1 - 1 >= 0 && 11 > kord2 - 1 >= 0)
+            {
+                cout << "w jakim kierunku ma byc skierowany? (wpisz liczbe)" << endl << "w gore - 1" << endl << "w dol - 2" << endl << "w lewo - 3" << endl << "w prawo - 4" << endl;
+                cin >> kierunek;
+
+                kord1v2 = kord1;
+                kord2v2 = kord2;
+
+                switch (kierunek)
+                {
+                case 1:
+                    if (gracz2[kord1v2 - 2][kord2v2 - 1] == 0)
+                    {
+                        if (kord1v2 - 2 >= 0)
+                        {
+                            kord1v2 -= 2;
+                            kord2v2 -= 1;
+
+                            statekv2gut = true;
+                        }
+                        else
+                        {
+                            cout << "Nie mozesz umiejscic statku poza mapa" << endl;
+                        }
+                    }
+                    else
+                    {
+                        cout << "Tam juz jest statek!" << endl;
+                    }
+                    break;
+                case 2:
+                    if (gracz2[kord1v2][kord2v2 - 1] == 0)
+                    {
+                        if (kord1v2 >= 0)
+                        {
+                            kord2v2 -= 1;
+
+                            statekv2gut = true;
+                        }
+                        else
+                        {
+                            cout << "Nie mozesz umiejscic statku poza mapa" << endl;
+                        }
+                    }
+                    else
+                    {
+                        cout << "Tam juz jest statek!" << endl;
+                    }
+                    break;
+                case 3:
+                    if (gracz2[kord1v2 - 1][kord2v2 - 2] == 0)
+                    {
+                        if (kord2v2 - 2 >= 0)
+                        {
+                            kord1v2 -= 1;
+                            kord2v2 -= 2;
+
+                            statekv2gut = true;
+                        }
+                        else
+                        {
+                            cout << "Nie mozesz umiejscic statku poza mapa" << endl;
+                        }
+                    }
+                    else
+                    {
+                        cout << "Tam juz jest statek!" << endl;
+                    }
+                    break;
+                case 4:
+                    if (gracz2[kord1v2 - 1][kord2v2] == 0)
+                    {
+                        if (kord2v2 >= 0)
+                        {
+                            kord1v2 -= 1;
+
+                            statekv2gut = true;
+                        }
+                        else
+                        {
+                            cout << "Nie mozesz umiejscic statku poza mapa" << endl;
+                        }
+                    }
+                    else
+                    {
+                        cout << "Tam juz jest statek!" << endl;
+                    }
+                    break;
+                default:
+                    break;
+                }
+            }
+            else
+            {
+                cout << "Nie mozesz umiejscic statku poza mapa" << endl;
+            }
+
+            if (statekv2gut)
+            {
+                gracz2[kord1 - 1][kord2 - 1] = 2;
+                gracz2[kord1v2][kord2v2] = 2;
+
+                statekv2gut = false;
+
+                progres++;
+            }
+        }
+        else
+        {
+            cout << "Tam juz jest statek!" << endl;
+        }
+
+    }
+
+
+}
+
+
+int main()
+{
+    cout << "Wpisz nazwe pierwszego gracza" << endl;
+    cin >> imie1;
+
+    cout << "Wpisz nazwe drugiego gracza" << endl;
+    cin >> imie2;
+
+    Rozstawienie1();
+    Rozstawienie2();
 
     Sleep(2000);
     system("cls");
-    cout << "Tak ostatecznie wyglada mapka" << endl;
+    cout << "Tak ostatecznie wygladaja mapki" << endl;
     Mapa(1);
+    Mapa(2);
 }
